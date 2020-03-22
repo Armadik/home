cbaf277254c8e32833bcc45d19b983dd10721486
test <test>
2020-03-22 17:28:34 +0300

    start1.14

--- test/13_1_def_arifm.py
+++ test/13_1_def_arifm.py
@@ -1,1 +1,16 @@
+def myfunc(func, *args):
+    return func(*args)
+
+
+print(myfunc(lambda x, y: x*y, 3, 4))
+print(myfunc(lambda x: x/100, 100))
+print(myfunc(lambda x: x*x*x, 4))
+print(myfunc(lambda x, y: x**y, 9, 3))
+
+def my_func2(*args):
+    try:
+        result = eval(args[0])
+    except SyntaxError:
+        result = "Всё же самому нужно подумать"
+    return result
 
