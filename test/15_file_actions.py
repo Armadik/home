d7d8f1e043bcbd2e53d73c0dd5f062fe26f6f860
test <test>
2020-03-24 01:12:18 +0300

    получил список валидных телефонов, имя и фамилию.

--- test/15_file_actions.py
+++ test/15_file_actions.py
@@ -9,7 +9,14 @@ def email_gen(list_of_names):
         emails.append(i[1] + '.' + i[0][0:letter] + '@company.io')
     return emails
 
+
+def spisok_name(a, b):
+    nl = []
+    for d, f in zip(a, b):
+        nl.append([d[1:], f[1:]])
+        return nl
 #################################################################
+
 
 with io.open('task_file.txt', 'r') as file:
     for line in file:
@@ -18,12 +25,10 @@ with io.open('task_file.txt', 'r') as file:
         last_name = (words[2::4])
         tel = (words[3::4])
         city = (words[4::4])
+        for t, n, l in zip(tel, name, last_name):
+            if t[0:2] == ' 7':
+                print(t, n, l)
+                print(email_gen(spisok_name([n], [l])))
 
-index = 0
-for d, f in zip(name, last_name):
-    index += 1
-#    print(d+'_'+str(index), f+'_'+str(index))
-    nl = []
-    nl.append(d[1:]+'_'+str(index)+','+f+'_'+str(index))
-    print(nl)
+
 
