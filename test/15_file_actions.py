285c97b357b2d5037326324b8e84426343da6d4c
test <test>
2020-03-25 18:04:27 +0300

    + test-test.2

--- test/15_file_actions.py
+++ test/15_file_actions.py
@@ -1,6 +1,6 @@
 import io
 
-
+################################################################
 def email_gen(list_of_names):
     emails = []
     for i in list_of_names:
@@ -12,6 +12,7 @@ def email_gen(list_of_names):
 
 
 def spisok_name(a, b):
+    '''Comment :)'''
     nl = []
     for d, f in zip(a, b):
         nl.append([d[1:], f[1:]])
@@ -26,11 +27,13 @@ with io.open('task_file.txt', 'r') as file:
         last_name = (words[2::4])
         tel = (words[3::4])
         city = (words[4::4])
+        with open('test-test', 'w') as new_file:
+            new_file.write(str(line))
         for t, n, l, c in zip(tel, name, last_name, city):
             if t[0:2] == ' 7':
                 mail = email_gen(spisok_name([n], [l]))
                 lin1 = (str(mail[0]), t, n, l, c[:-1])
-                with open('test-test', 'w+') as new_file:
+                with open('test-test', 'a') as new_file:
                     new_file.write(str(lin1) + '\n')
             else:
                 lin2 = ('no mail', t, n, l, c[:-1])
