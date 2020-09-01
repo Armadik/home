bcf076f2d06cc7790efb2e07664e20c090b7176d
test <test>
2020-09-01 21:21:17 +0300

    +coursera

--- coursera_python/solution.py
+++ coursera_python/solution.py
@@ -1,13 +1,1 @@
-a, b, c = int(input()), int(input()), int(input())
-if a == min(a, b, c) and c == max(a, b, c):
-    print(a, b, c)
-elif c == min(a, b, c) and a == max(a, b, c):
-    print(c, b, a)
-elif b == min(a, b, c) and c == max(a, b, c):
-    print(b, a, c)
-elif b == min(a, b, c) and a == max(a, b, c):
-    print(b, c, a)
-elif b == max(a, b, c) and c == min(a, b, c):
-    print(c, a, b)
-elif b == max(a, b, c) and a == min(a, b, c):
-    print(a, c, b)
+
