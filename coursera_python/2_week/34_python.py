5f0a8452944d4bc6984f3d4d63fe79aabddf356f
test <test>
2020-09-13 17:48:05 +0300

    +coursera

--- coursera_python/2_week/34_python.py
+++ coursera_python/2_week/34_python.py
@@ -1,17 +1,26 @@
 old = int(input())
-i_b = 0
-max_b = 0
+i_b = 1
+i_n = 1
+max_b = 1
+max_n = 1
 n = int(input())
 while n != 0:
-    if n > old or n < old:
+    if n > old:
         i_b += 1
-        print(max_b, i_b)
         if max_b < i_b:
             max_b = i_b
+    elif n < old:
+        i_n = 1
+        if max_n < i_n:
+            max_n = i_n
     if n == old:
-        i_b = 0
+        i_b = 1
+        i_n = 1
     old = n
     n = int(input())
     if n == 0:
         break
-print(max_b)
+if max_b >= max_n:
+    print(max_b)
+else:
+    print(max_n)
