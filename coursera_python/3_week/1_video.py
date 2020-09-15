e2c2f82753e6383300acdf45ef558918bb59016c
test <test>
2020-09-15 20:17:57 +0300

    +coursera

--- coursera_python/3_week/1_video.py
+++ coursera_python/3_week/1_video.py
@@ -1,1 +1,9 @@
 print('{0:.25f}'.format(0.1))
+print(0.2.as_integer_ratio())
+x = float(input())
+y = float(input())
+epsilon = 10 ** -6
+if abs(x - y) < epsilon:
+    print('Equal')
+else:
+    print('Not equal')
--- coursera_python/solution.py
+++ coursera_python/solution.py
@@ -1,1 +1,2 @@
 print('{0:.25f}'.format(0.1))
+print(0.5.as_integer_ratio())
