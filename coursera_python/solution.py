2b4acc23c5ea58339f7d55740b9e827ae9719e1d
test <test>
2020-09-16 20:40:46 +0300

    +coursera

--- coursera_python/solution.py
+++ coursera_python/solution.py
@@ -1,9 +1,25 @@
-x = float(input())
-r = float(input())
-k = float(input())
-cena = r + (k/100)
-pribil = cena * (x / 100)
-summa = cena + pribil
-kopeek = (summa % 1) * 100
-ruble = int(summa)
-print(ruble, '{0:.0f}'.format(kopeek))
+#x = float(input())
+#r = float(input())
+#k = float(input())
+x = 12
+r = 179
+k = 0
+gods = 5
+za_god = 0
+i = 0
+while i != gods:
+    cena = r + (k/100)
+    print(cena)
+    pribil = cena * (x / 100)
+    print(pribil)
+    summa = cena + pribil
+    print(summa)
+    i += 1
+    cena += summa
+    print(cena)
+
+#kopeek = (summa % 1) * 100
+#ruble = int(summa)
+#za_god = ruble + (summa % 1)
+
+
