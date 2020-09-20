e8d1f7cad80e5cd83c77e3069f39f86beefc575d
test <test>
2020-09-20 20:14:40 +0300

    +coursera

--- coursera_python/3_week/13_python.py
+++ coursera_python/3_week/13_python.py
@@ -1,10 +1,10 @@
 string = str(input())
-obr = string[::-1]
-pos = string.find("h")
-sop = obr.find("h")
-if pos and len(string) - sop - 1 > 0:
-    print(string[0:pos], string[(len(string) - sop):-1], string[-1], sep='')
-elif pos == -1:
+p = string.find("h")
+s = string[::-1].find("h")
+if p and len(string) - s - 1 > 0:
+    print(string[0:len(string) - s - 1], string[p + 1:-1], string[-1], sep='')
+elif string[0] and string[-1] == "h":
+    print(string)
     pass
 else:
     pass
