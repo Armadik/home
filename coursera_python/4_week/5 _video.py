e9df186926713d6c30f4dc9739bb3f0b15f28259
test <test>
2020-09-23 22:29:01 +0300

    +coursera

---
+++ coursera_python/4_week/5 _video.py
@@ -0,0 +1,31 @@
+def sort2(a, b):
+    if a < b:
+        return a, b
+    else:
+        return b, a
+
+
+a = int(input())
+b = int(input())
+minimum, maximum = sort2(a, b)
+print(minimum, maximum)
+
+
+def max2(a, b):
+    if a > b:
+        return a
+    return b
+
+
+def max3(a, b, c):
+    return max2(max2(a, b), c)
+
+
+def isEven(n):
+    return n % 2 == 0
+
+
+if isEven(n):
+    print("EVEN")
+else:
+    print("ODD")
